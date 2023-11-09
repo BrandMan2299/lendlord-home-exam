eb5d68a63557e161190cd0c7e53258ece2064619
itai brand <67857476+BrandMan2299@users.noreply.github.com>
2023-11-09 17:32:40 +0200

    backend up

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose')
 
-const collectionName = 'users'
-const schemaName = 'users'
-const SchemaTypes = mongoose.Schema
+const collectionName = 'users';
+const schemaName = 'users';
+const SchemaTypes = mongoose.Schema;
 
-const schema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
-    _id: { type: String, auto: true },
     firstName: { type: String, required: true, trim: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
-    dateStarted: { type: Date, required: true },
-    salary: { type: String, required: true },
+    dateStarted: { type: Date, default: Date.now },
+    salary: { type: Number, required: true },
+    manager: { type: String },
+    role: { type: String, enum: ['Manager', 'Worker', 'Driver'], required: true }
   },
-  { strict: false, autoCreate: true, timestamps: true, versionKey: false }
+  { strict: true, autoCreate: true, timestamps: true, versionKey: false }
 )
+const User = mongoose.model(schemaName, userSchema, collectionName);
 
-const model = mongoose.model(schemaName, schema, collectionName)
-
-module.exports = model
-module.exports.schema = schema
+module.exports = User;
+module.exports.userSchema = userSchema;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -5,9 +5,18 @@ const User = require('../models/users');
 
 router.post('/create', async (ctx) => {
     const user = new User(ctx.request.body);
-    await user.save();
-    const firstName = (await User.findOne({ createdAt: user.createdAt })).firstName;
-    ctx.response.body = `Hello ${firstName} ${user.lastName}, welcome aboard!`;
+    if (user.role != 'Manager') {
+        try {
+            const manager = await User.findById(user.manager);
+            await user.save();
+        } catch (error) {
+            ctx.status(422);
+            ctx.body = "wrong manager Id..."
+        }
+    } else {
+        await user.save();
+        ctx.body = `Hello ${user.firstName} ${user.lastName}, welcome aboard!`;
+    }
 });
 
 router.get('/getAllUsers', async (ctx) => {
@@ -18,6 +27,7 @@ router.get('/getAllUsers', async (ctx) => {
 router.get('/getUserById/:id', async (ctx) => {
     const user = await User.findById(ctx.params.id);
     ctx.body = user;
+
 })
 
 router.post('/update', async (ctx) => {
@@ -26,12 +36,19 @@ router.post('/update', async (ctx) => {
     await User.findOneAndUpdate(query, data);
     const user = await User.findOne(data);
     ctx.body = user;
+
 })
 
 router.delete('/delete', async (ctx) => {
     const query = ctx.query;
     const user = await User.findOneAndDelete(query);
     ctx.body = user;
+})
+
+router.get('/getManagerAndEmployees/:id', async (ctx) => {
+    const manager = await User.findById(ctx.params.id);
+    const employees = await User.find({ manager: ctx.params.id });
+    ctx.body = { manager, length: employees.length, employees };
 })
 
 router.allowedMethods()
